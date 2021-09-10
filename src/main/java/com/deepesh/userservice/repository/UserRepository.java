35ecb90d0e8fe80048435f5fb935c48439cdf9f3
Deepesh Subedi <dsubedi1130@gmail.com>
2021-09-10 17:43:27 -0500

    Creating repository to access database creating rest controller class to handle all client requests

--- src/main/java/com/deepesh/userservice/repository/UserRepository.java
+++ src/main/java/com/deepesh/userservice/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.deepesh.userservice.repository;
 
-public interface UserRepository {
+import com.deepesh.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
