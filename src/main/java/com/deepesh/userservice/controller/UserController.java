3eee226868250c82ff794da63f79bc7d3cf1d826
Deepesh Subedi <dsubedi1130@gmail.com>
2021-09-10 19:35:40 -0500

    Cross-origin problem occurs because react and tomcat runs on different server. hence, we need to give access by doing @CrossOrigins (origin="localhost://p_n")

--- src/main/java/com/deepesh/userservice/controller/UserController.java
+++ src/main/java/com/deepesh/userservice/controller/UserController.java
@@ -3,12 +3,13 @@ package com.deepesh.userservice.controller;
 import com.deepesh.userservice.entity.User;
 import com.deepesh.userservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api")
 public class UserController {
