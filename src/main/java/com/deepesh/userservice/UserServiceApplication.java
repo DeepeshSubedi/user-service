e8655f78cbc9f4955ad6660015bf5f1deb7a3a23
Deepesh Subedi <dsubedi1130@gmail.com>
2021-09-10 19:42:27 -0500

    used 3 args constructors

--- src/main/java/com/deepesh/userservice/UserServiceApplication.java
+++ src/main/java/com/deepesh/userservice/UserServiceApplication.java
@@ -19,9 +19,9 @@ public class UserServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        this.userRepository.save(new User(1l,"Deepesh","Subedi","Deepesh@gmail.com"));
-        this.userRepository.save(new User(2l,"Rama","Subedi","rama@gmail.com"));
-        this.userRepository.save(new User(3l,"Ashmita","Subedi","ashmita@gmail.com"));
+        this.userRepository.save(new User("Deepesh","Subedi","Deepesh@gmail.com"));
+        this.userRepository.save(new User("Rama","Subedi","rama@gmail.com"));
+        this.userRepository.save(new User("Ashmita","Subedi","ashmita@gmail.com"));
 
     }
 
--- src/main/java/com/deepesh/userservice/entity/User.java
+++ src/main/java/com/deepesh/userservice/entity/User.java
@@ -13,11 +13,17 @@ import javax.persistence.*;
 @ToString
 public class User {
     @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(name="first_name")
     private String firstName;
     @Column(name="last_name")
     private String lastName;
     private String email;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
